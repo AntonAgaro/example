371b3be372ecd5dca9b8700e95d7a3fca46af91d
Anton <example@mail.com>
2021-02-23 18:29:21 +0400

    promo_section media

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //Выплывающее меню
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.menu');
+    
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });      
+
+     
+          
+});
