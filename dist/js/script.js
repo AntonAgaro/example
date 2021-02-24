2411007a8f4f41d5f047b2f8a052e2e8bf4c3ecf
Anton <example@mail.com>
2021-02-24 17:15:53 +0400

    slider

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,8 +6,39 @@ document.addEventListener('DOMContentLoaded', () => {
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
-    });      
+    });   
+    
+    //Slider
+    const slides = document.querySelectorAll('.reviews__slider-wrapper_client'),
+          prev = document.querySelector('.reviews__slider-prev'),
+          next = document.querySelector('.reviews__slider-next');
+    let slideIndex = 1;
+    
+    showSlides(slideIndex);
 
-     
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = "none");
+
+        slides[slideIndex - 1].style.display = "block";
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
           
 });
